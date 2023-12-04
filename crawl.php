a5c1b65b6f1de8cb0210ee896966ea04785c90f5
Syed Ahsan Ullah Tanweer <102422437+Me-AU@users.noreply.github.com>
2023-12-04 23:48:19 +0500

    Update crawl.php
    allowed CORS

--- crawl.php
+++ crawl.php
@@ -1,5 +1,16 @@
 <?php
 
+// Allow CORS (Cross-Origin Resource Sharing)
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Handle pre-flight CORS requests (OPTIONS method)
+    http_response_code(200);
+    exit();
+}
+
 if (isset($_POST['seedUrl'])) {
     $seedUrl = $_POST['seedUrl'];
 
